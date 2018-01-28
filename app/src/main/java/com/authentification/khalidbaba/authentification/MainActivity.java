23cc58f7c176683bdd34e45b1d069795be1871a5
khalidbaba <baba.khaliid@gmail.com>
2018-01-28 15:26:14 +0000

    Initial commit

---
+++ app/src/main/java/com/authentification/khalidbaba/authentification/MainActivity.java
@@ -0,0 +1,35 @@
+package com.authentification.khalidbaba.authentification;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by khalidbaba on 28/01/2018.
+ */
+
+
+public class MainActivity extends Activity {
+
+
+    // TODO : declare all variables (elements AND variables)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //TODO Instantiate object (EditText and Button)
+
+
+
+        //TODO : handle click listener in button and get values from the editText and affect it to two variables
+
+
+        //TODO : Create Intent and send email content to the welcome_activity
+
+
+
+    }
+}
---
+++ app/src/main/java/com/authentification/khalidbaba/authentification/WolcomeActivity.java
@@ -0,0 +1,25 @@
+package com.authentification.khalidbaba.authentification;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by khalidbaba on 28/01/2018.
+ */
+
+public class WolcomeActivity extends Activity {
+
+    //TODO : Declare all variables (TextView, String)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        //TODO : Instantiate all objects
+
+
+        //TODO  : Get the intent content and show the Email in the TextView
+    }
+}
